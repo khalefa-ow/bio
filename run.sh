74872b3837581962b8959e4b7b799f8cf093b912
khalefa-ow <88914125+khalefa-ow@users.noreply.github.com>
2022-02-12 21:57:20 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@
 
 FILE=${1?Error: No filename given.}
 CORE=${2?Error: No core given.}
+echo "file: $FILE"
+echo "core; $CORE"
+
 /usr/bin/time -v --output=all.txt ./TERAD1 $FILE $CORE ./arthro_ES_ND_PV_classified.fa none
+
+./combine.sh
+
+
+./convert.py
+
+
+mv f.csv process_${FILE}__${CORE}.csv
