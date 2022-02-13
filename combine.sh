bcb7d392540b725b7c15b7a151f31f3f202906d0
khalefa-ow <88914125+khalefa-ow@users.noreply.github.com>
2022-02-13 15:38:02 -0500

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 printf "\n step: 1 \n"
-cat s1.txt
+[ -f s1.tmp ] && cat s1.tmp
 printf "\n step: 3 \n"
-cat s31.txt
-[ -f 32.txt ] && cat 32.txt
+[ -f s31.tmp ] && cat s31.tmp
+[ -f s32.tmp ] && cat s32.tmp
 printf "\n step: 4 \n"
-cat s4.txt
+[ -f s4.tmp ] && cat s4.tmp
 printf "\n step: 5 \n"
-cat s5.txt
+[ -f s5.tmp ] && cat s5.txt
 printf '\n step: "all" \n'
-cat all.txt
+[ -f all.tmp ] && cat all.tmp
